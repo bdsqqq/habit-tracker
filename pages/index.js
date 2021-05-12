a6aa5dd6b4a153297a709484c6fc76953f142422
Igor Bedesqui <igorbedesqui@gmail.com>
2021-05-11 22:19:34 -0300

    feat: added gql query to getStaticProps and log

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,35 @@
-export default function Home() {
+import { useEffect } from "react";
+
+export default function Home({ habit_instances }) {
+  useEffect(() => {
+    let instances = habit_instances.habit_tracker_habit_instance;
+    instances.map((instance) => {
+      console.log(instance.value, instance.instance_date);
+    });
+  }, [habit_instances]);
   return <div>Hej do</div>;
 }
+
+export async function getStaticProps() {
+  const { data } = await client.query({
+    query: gql`
+      query MyQuery {
+        habit_tracker_habit_instance {
+          id
+          habit_id
+          instance_date
+          value
+        }
+      }
+    `,
+  });
+
+  return {
+    props: {
+      habit_instances: data,
+    },
+  };
+}
+
+import { gql } from "@apollo/client";
+import client from "../apollo-client";
